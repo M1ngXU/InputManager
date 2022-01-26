4da24ab4dcb77a7a29f7f707895dbab5ce774303
M1ngXU <max.obreiter@gmail.com>
2022-01-26 19:05:19 +0100

    fixed bugs

--- src/bin/keyboard_event_test_simulator.rs
+++ src/bin/keyboard_event_test_simulator.rs
@@ -5,8 +5,8 @@ use winsafe_input_manager::sender::*;
 
 fn main() -> WindowsResult {
 	KeyboardEvent::VirtualKeyDown(VirtualKey::VolumeMute).into_event_queue().execute()?;
-	input_manager::listener::init();
-	for event in input_manager::listener::get_event_receiver().into_iter() {
+	winsafe_input_manager::listener::init();
+	for event in winsafe_input_manager::listener::get_event_receiver().into_iter() {
 		if let Some(EventType::KeyboardEvent(k)) =  event.into_event() {
 			println!("{:?}", k);
 		}
